c00422b8375ed5d08a20b80eac11a48bfde8687e
Alexander Palacios <alexsanchez366.ap@gmail.com>
2024-01-15 13:30:49 -0600

    agregar nuevo vuelo

---
+++ src/services/diaryServices.ts
@@ -0,0 +1,39 @@
+import { DiaryEntry, NonSensitiveInfoDiaryEntry, newDiaryEntry  } from '../types'
+import diaryData from './diaries'
+
+
+const diaries: DiaryEntry[] = diaryData as Array<DiaryEntry>
+
+export const getEntries = (): DiaryEntry[] => diaries
+
+export const findById= (id:number): DiaryEntry | undefined=>{
+    const entry = diaries.find(d => d.id ===id)
+    return entry
+}
+
+
+export const getEntriesWithoutSensitiveInfo = (): NonSensitiveInfoDiaryEntry [] => {
+    return diaries.map(({id, date, weather, visibility}) =>{
+        return{
+        id ,
+        date ,
+        weather ,
+        visibility
+                }
+                                                            })
+                                                                                    }       
+
+
+
+export const addDiary =(newDiaryEntry: newDiaryEntry ): DiaryEntry =>{
+    const newDiary ={
+        id: Math.max(...diaries.map(d => d.id))+1,
+        ...newDiaryEntry
+    }
+
+    diaries.push(newDiary)
+    return newDiary
+} 
+
+
+
